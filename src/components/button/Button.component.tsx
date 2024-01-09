d0066eef303984b0287a5e8dbfae0e6352b46148
AlexNixx <alexnix.mail@gmail.com>
2024-01-10 00:53:35 +0200

    feat: add button

---
+++ src/components/button/Button.component.tsx
@@ -0,0 +1,70 @@
+import { memo } from 'react';
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
+
+import { classNames } from '../../utils/classNames';
+import type { Mods } from '../../utils/classNames';
+
+import cls from './Button.module.scss';
+
+type ButtonSizes = 'sm' | 'md' | 'lg';
+type ButtonVariants = 'solid' | 'outlined' | 'clear';
+type ButtonColors = 'primary' | 'secondary' | 'success' | 'warning' | 'danger';
+type ButtonRadius = 'none' | ButtonSizes;
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  size?: ButtonSizes;
+  variant?: ButtonVariants;
+  color?: ButtonColors;
+  radius?: ButtonRadius;
+  isLoading?: boolean;
+  fullWidth?: boolean;
+  disabled?: boolean;
+  startContent?: ReactNode;
+  endContent?: ReactNode;
+  children: ReactNode;
+}
+
+export const Button = memo((props: ButtonProps) => {
+  const {
+    className,
+    size = 'md',
+    variant = 'solid',
+    color = 'primary',
+    radius = 'md',
+    isLoading = false,
+    fullWidth = false,
+    disabled = false,
+    startContent,
+    endContent,
+    children,
+    ...otherProps
+  } = props;
+
+  const isDisabled = isLoading || disabled;
+
+  const mods: Mods = {
+    [cls[color]]: variant === 'solid',
+    [cls.disabled]: isDisabled,
+    [cls.fullWidth]: fullWidth
+  };
+
+  const additional = [
+    className,
+    cls[variant],
+    cls[`size_${size}`],
+    cls[`radius_${radius}`]
+  ];
+
+  return (
+    <button
+      disabled={isDisabled}
+      className={classNames(cls.button, mods, additional)}
+      {...otherProps}
+    >
+      {startContent}
+      {children}
+      {endContent}
+    </button>
+  );
+});
