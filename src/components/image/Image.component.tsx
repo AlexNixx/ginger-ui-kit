1e4b61a4607ea060489c5249bdb62e905c38ba52
Alex Nixx <alexnix.mail@gmail.com>
2024-03-03 23:07:59 +0200

    fix(image): fix image styles, add fallback url

--- src/components/image/Image.component.tsx
+++ src/components/image/Image.component.tsx
@@ -1,10 +1,13 @@
 import { memo, useLayoutEffect, useState } from 'react';
 import type { ImgHTMLAttributes, ReactElement } from 'react';
 
+import { classNames } from 'shared/utils';
 import type { BorderRadius } from 'model';
 import { classNames } from 'utils';
 
 import cls from './Image.module.scss';
+
+type ObjectFit = 'contain' | 'cover' | 'fill' | 'none' | 'scale-down';
 
 interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
   className?: string;
@@ -18,7 +21,9 @@ export const _Image = memo((props: ImageProps) => {
     className,
     src,
     radius = 'md',
+    objectFit = 'cover',
     fallback,
+    fallbackUrl,
     errorFallback,
     alt,
     ...otherProps
@@ -41,15 +46,38 @@ export const _Image = memo((props: ImageProps) => {
     };
   }, [src]);
 
+  const additional: string[] = [
+    className,
+    cls[`radius_${radius}`],
+    cls[`objectFit_${objectFit}`]
+  ];
+  const fallbackClassName = classNames(cls.imageFallback, {}, additional);
+  const imageClassName = classNames(cls.image, {}, additional);
+  const style = { height, width };
+
   if (isLoading && fallback) return fallback;
 
   if (isError && errorFallback) return errorFallback;
 
   return (
-    <img
-      className={classNames(cls.image, {}, additional)}
-      alt={alt}
-      {...otherProps}
-    />
+    <>
+      {fallbackUrl && isLoading && (
+        <img
+          className={fallbackClassName}
+          style={style}
+          alt={alt}
+          src={fallbackUrl}
+          {...otherProps}
+        />
+      )}
+      <img
+        className={imageClassName}
+        style={style}
+        alt={alt}
+        loading='lazy'
+        src={src}
+        {...otherProps}
+      />
+    </>
   );
 });
