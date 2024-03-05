427512eebc8e917efb3a8c229490061076c8bb00
Alex Nixx <alexnix.mail@gmail.com>
2024-03-05 23:14:43 +0200

    fix(image): update image styles, add fallbackUrl

--- src/components/image/Image.component.tsx
+++ src/components/image/Image.component.tsx
@@ -1,9 +1,8 @@
-import { memo, useLayoutEffect, useState } from 'react';
-import type { ImgHTMLAttributes, ReactElement } from 'react';
+import { memo, ReactNode, useLayoutEffect, useState } from 'react';
+import type { ImgHTMLAttributes } from 'react';
 
 import { classNames } from 'shared/utils';
 import type { BorderRadius } from 'model';
-import { classNames } from 'utils';
 
 import cls from './Image.module.scss';
 
@@ -12,14 +11,18 @@ type ObjectFit = 'contain' | 'cover' | 'fill' | 'none' | 'scale-down';
 interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
   className?: string;
   radius?: BorderRadius;
-  fallback?: ReactElement;
-  errorFallback?: ReactElement;
+  objectFit?: ObjectFit;
+  fallback?: ReactNode;
+  fallbackUrl?: string;
+  errorFallback?: ReactNode;
 }
 
 export const _Image = memo((props: ImageProps) => {
   const {
     className,
     src,
+    width,
+    height,
     radius = 'md',
     objectFit = 'cover',
     fallback,
@@ -28,8 +31,6 @@ export const _Image = memo((props: ImageProps) => {
     alt,
     ...otherProps
   } = props;
-
-  const additional: string[] = [className, cls[`radius_${radius}`]];
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isError, setIsError] = useState<boolean>(false);
---
+++ src/components/image/Image.stories.tsx
@@ -0,0 +1,89 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { _Image as Image } from './Image.component';
+import { Skeleton } from '../skeleton';
+import { Typography } from '../typography';
+import cls from '../checkbox/Checkbox.module.scss';
+
+const meta: Meta<typeof Image> = {
+  component: Image,
+  title: 'Components/Image',
+  tags: ['autodocs']
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const successfulUrl =
+  'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg';
+const largeUrl =
+  'https://upload.wikimedia.org/wikipedia/commons/c/cc/ESC_large_ISS022_ISS022-E-11387-edit_01.JPG';
+const errorUrl = 'https://example.com/nonexistent-image.jpg';
+const smallUrl =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb' +
+  '/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png';
+
+export const Default: Story = {
+  args: {},
+  render: args => (
+    <Image height={500} width={500} radius='md' src={successfulUrl} {...args} />
+  )
+};
+
+export const ErrorFallbacks: Story = {
+  args: {},
+  render: args => (
+    <Image
+      height={500}
+      width={500}
+      radius='md'
+      fallback={<Skeleton width={500} height={500} />}
+      errorFallback={
+        <div
+          style={{
+            height: 500,
+            width: 500,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: 'var(--grey-300)',
+            borderRadius: 'var(--border-radius-sm)'
+          }}
+        >
+          <Typography>We're sorry, but we couldn't load your image</Typography>
+        </div>
+      }
+      src={errorUrl}
+      {...args}
+    />
+  )
+};
+
+export const LoadingFallbacks: Story = {
+  args: {},
+  render: args => (
+    <Image
+      height={500}
+      width={500}
+      radius='md'
+      fallback={<Skeleton width={500} height={500} />}
+      src={largeUrl}
+      {...args}
+    />
+  )
+};
+
+export const FallbackUrl: Story = {
+  args: {},
+  render: args => (
+    <Image
+      height={500}
+      width={500}
+      radius='md'
+      fallbackUrl={smallUrl}
+      src={largeUrl}
+      {...args}
+    />
+  )
+};
